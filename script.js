29425361c495a184f62e2e36adfa7502c2fb55b0
metehan <metehansazak@gmail.com>
2022-11-17 12:50:11 +0800

    add color and black options
    finish except it can be styled with css

--- script.js
+++ script.js
@@ -16,19 +16,68 @@ boxes.style.height = heightBox;
 boxes.style.width = heightWidth;
 }
 
+const btnColor = document.querySelector('#color');
+btnColor.addEventListener('click', function() {
+    while (container.firstChild) {
+        container.firstChild.remove();};
+        container.style.backgroundColor = 'white';
+
+for (let i = 0; i < (initHeight * initWidth) ; i++){
+    let boxes = document.createElement('div');
+    boxes.classList.add('boxes');
+    container.appendChild(boxes);
+    boxes.style.backgroundColor = 'white'; 
+    boxes.style.height = `${parseInt(container.style.height)/initHeight}px`;
+    boxes.style.width = `${parseInt(container.style.width)/initWidth}px`;
+};
+    container.addEventListener('mouseover', function(e)
+    {
+        e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    });
+});
+
+const btnBlack = document.querySelector('#black');
+btnBlack.addEventListener('click', function() {
+    while (container.firstChild) {
+        container.firstChild.remove();};
+        container.style.backgroundColor = 'white';
+
+for (let i = 0; i < (initHeight * initWidth) ; i++){
+    let boxes = document.createElement('div');
+    boxes.classList.add('boxes');
+    container.appendChild(boxes);
+    boxes.style.backgroundColor = 'white'; 
+    boxes.style.height = `${parseInt(container.style.height)/initHeight}px`;
+    boxes.style.width = `${parseInt(container.style.width)/initWidth}px`;
+};
+    container.addEventListener('mouseover', function(e)
+    {
+        e.target.style.backgroundColor = 'black';
+    });
+});
+
+
+
+
+function randomColor(){
+    let color = Math.floor(Math.random() * 256)
+    return color;
+};
+
+
+
 container.addEventListener('mouseover', function(e)
 {
     e.target.style.backgroundColor = 'black';
 });
 
-const btnReset = document.querySelector('.button');
+const btnReset = document.querySelector('#reset');
 
 btnReset.addEventListener('click', function(){
    let input = prompt('number of squares per size?');
    if (input <= 100) {
    let initHeight = input;
    let initWidth = input;
-  
    while (container.firstChild) {
     container.firstChild.remove()
 };
@@ -46,4 +95,4 @@ for (let i = 0; i < (initHeight * initWidth) ; i++){
 }} else alert('please choose a number less than 100');
 });
 
-page.appendChild(btnReset);
+
